b6c97ede6a8c3c5ab818b021f3b8eac6d9174ea9
Daniel Beaver <daniel.beaver1012@gmail.com>
2020-03-26 19:07:37 -0400

    recommit

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,34 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-
- class WeatherApi extends Component{
-constructor(){
+class WeatherApi extends Component {
+  constructor() {
     super();
-    this.state={
-        data:null
-    }
-this.getData()
-}
+    this.state = {
+      data: null
+    };
+  }
 
-// https://openweathermap.org/current Api doc link 
+  // https://openweathermap.org/current Api doc link
 
-//fetchs data from api based on lon & lat.  Daleville indiana lat and lon in currently 
+  //fetchs data from api based on lon & lat.  Daleville indiana lat and lon in currently
 
-getData(){
-    let data = fetch('api.openweathermap.org/data/2.5/weather?lat=40.1212&lon=85.5580&appid=f340f1de73f3193cb855cbcda23ece38')
-    .then((res)=>{
-      res.json()
-        console.log(res);
-      
-    })
-}
-    render(){
-        return(
-            <div>
-
-            </div>
-        )
-    }
+  componentDidMount() {
+    fetch(
+      "api.openweathermap.org/data/2.5/weather?lat=40.1212&lon=85.5580&appid=f340f1de73f3193cb855cbcda23ece38"
+    ).then(res => {
+      res.json();
+      console.log(res);
+    });
+  }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default WeatherApi;

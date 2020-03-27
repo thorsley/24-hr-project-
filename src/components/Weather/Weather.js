8acdd0bb58021accd0ba1f8d02c624cf785733be
Daniel Beaver <daniel.beaver1012@gmail.com>
2020-03-27 10:16:41 -0400

    daily push

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -8,25 +8,41 @@ class WeatherApi extends React.Component {
     this.state = {
       temp: "",
       lon: props.lon,
-      lat: props.lat
+      lat: props.lat,
+      unit: true
     };
   }
+
   componentDidMount() {
     //&units=Imperial fetchs the temp as F instead of C which is default.
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=Imperial&appid=${key}`
-    )
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          temp: json.main.temp
-        });
-      });
+    this.state.temp
+      ? fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=Imperial&appid=${key}`
+        )
+          .then(response => response.json())
+          .then(json => {
+            this.setState({
+              temp: json.main.temp
+            });
+          })
+      : fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=metric&appid=${key}`
+        )
+          .then(response => response.json())
+          .then(json => {
+            this.setState({
+              temp: json.main.temp
+            });
+          });
   }
+
   render() {
     return (
       <div>
         <p>{Math.round(this.state.temp)}</p>
+        <button onClick={(this.state.unit = !this.state.unit)}>
+          Toggle Temp f/c{" "}
+        </button>
       </div>
     );
   }

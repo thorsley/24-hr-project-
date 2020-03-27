e6ae49e98ad32270450872c873b3f39418d55242
Trevor Horsley <trevorhorsley122@gmail.com>
2020-03-27 11:55:24 -0400

    styled

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -6,25 +6,6 @@ import React from "react";
 let key ="f340f1de73f3193cb855cbcda23ece38"
 
 
-class WeatherApi extends React.Component{
-constructor(props){
-    super(props);
-    this.state={
-        temp:"",
-        lon: props.lon,
-        lat: props.lat,
-        
-    };
-    }
-componentDidMount() {
-
-
-//&units=Imperial fetchs the temp as F instead of C which is default. 
- fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&units=Imperial&appid=${key}`)
-    .then(response => response.json())
-    .then(json => {
-
-let key = "f340f1de73f3193cb855cbcda23ece38";
 
 class WeatherApi extends React.Component {
   constructor(props) {
@@ -50,10 +31,6 @@ class WeatherApi extends React.Component {
     });
 }
 
-
-
-
-    
     render(){
         return(
             <div>
@@ -63,17 +40,6 @@ class WeatherApi extends React.Component {
             </div>
         )
     }
-
-      });
   }
-  render() {
-    return (
-      <div>
-        <p>{Math.round(this.state.temp)}</p>
-      </div>
-    );
-  }
-
-}
 
 export default WeatherApi;
--- src/components/Zomato/Zomato.js
+++ src/components/Zomato/Zomato.js
@@ -5,14 +5,17 @@ import React, { Component } from "react";
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
-import { makeStyles } from '@material-ui/core/styles';
-import styles from './zomato.css'
+import style from './zomato.css'
 
 
 let key = '3cdb1375e382e6a6edb08436dad67617';
 const config = { headers: {'user-key': key} }; 
 
+
+
 class Zomato extends Component {
+   
+
 
     constructor(props) {
         super(props);
@@ -39,14 +42,14 @@ class Zomato extends Component {
     render() {
         return (
             <>
-            <div>
+            <div className='scroll' >
                 {/* <h1>Region: {this.state.zomatoObj.location.title}</h1> */}
                 {this.state.zomatoObj.map((food, key) => {
                     return (
-                        <div>
+                        <div >
 
                             <div key={key}>
-                            <Card>
+                            <Card className="cardBox">
                             <CardContent>
                                 <Typography className="titleCard" variant="h5" component="h2" >
                                Restaurant Name: {food.restaurant.name} </Typography>
@@ -54,7 +57,6 @@ class Zomato extends Component {
                                 <Typography variant="body2" component="p">
                                 {food.restaurant.location.address} </Typography>
                                 {/* <h5>{food.restaurant.cuisines}</h5> */}
-
                                 <Typography variant="body2" component="p">
                                 {food.restaurant.cuisines}</Typography>
                                 {/* <h5>Avg Cost for 2: ${food.restaurant.average_cost_for_two}</h5> */}
@@ -71,55 +73,7 @@ class Zomato extends Component {
         )
     }
 
-let key = "3cdb1375e382e6a6edb08436dad67617";
-const config = { headers: { "user-key": key } };
-
-class Zomato extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      zomatoObj: [],
-      lon: props.lon,
-      lat: props.lat
-    };
-  }
-  componentWillMount() {
-    fetch(
-      `https://developers.zomato.com/api/v2.1/geocode?lat=${this.state.lat}&lon=${this.state.lon}`,
-      config
-    )
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-        this.setState({
-          zomatoObj: json.nearby_restaurants
-        });
-      });
-  }
-  render() {
-    return (
-      <>
-        <div>
-          {/* <h1>Region: {this.state.zomatoObj.location.title}</h1> */}
-          {this.state.zomatoObj.map((food, key) => {
-            return (
-              <div>
-                <div key={key}>
-                  <h4>Restaurant Name: {food.restaurant.name}</h4>
-                  <h5>{food.restaurant.location.address}</h5>
-                  <h5>{food.restaurant.cuisines}</h5>
-                  <h5>
-                    Avg Cost for 2: ${food.restaurant.average_cost_for_two}
-                  </h5>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
-
 }
+
 
 export default Zomato;
